6e3208254164d313ae0ab114ff655f533c7149aa
Christopher <epickmarioelcrack@gmail.com>
2024-11-21 11:54:22 -0600

    localhost

---
+++ script.js
@@ -0,0 +1,5 @@
+function agregarComentario() {
+    let comentarioIngresado = document.getElementById("input-comentario").value
+    document.getElementById("parrafo-comentario").innerHTML = comentarioIngresado
+    
+}
